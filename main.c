e45382584bf8dfa9a391cb196fb53c8ab3c7a850
fedebuonco <federico.buonco@gmail.com>
2025-03-11 22:42:17 +0000

    Reading from process_vm_readv

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
 #define _GNU_SOURCE
+#include <errno.h>
 #include <fcntl.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <stdint.h>
 // Find current mapped regions using maps
 // Iterate through those regions and use process_vm_readv() to read them
 // Use memchr to find portion of memory
@@ -18,72 +16,83 @@
 #define MAX_LINES 500
 #define MAX_STR_LEN 500
 
-char * to_hex_string(uint64_t num)
-{
-  char * result = malloc(sizeof(char)*20);
-	sprintf(result, "%8.8x", (uint32_t)num);
-  return result;
+void process_range(const char *address_range, uintptr_t *start,
+                   uintptr_t *end) {
+  sscanf(address_range, "%lx-%lx", start, end);
 }
 
-
-uintptr_t *process_range(char *address_range) {
-  uintptr_t * current_map = malloc(2 * sizeof(uintptr_t));
-  char astart[20] = {0};
-  char aend[20] = {0};
-  int delim_index = -1;
-  int i = 0;
-
-  for (; i < strlen(address_range); i++) {
-      if (address_range[i] == '-') {
-          delim_index = i + 1;
-          strncpy(astart, address_range, i);
-          astart[i] = '\0';
-          break;
-      }
-  }
-  strncpy(aend, address_range + delim_index, i);
-  aend[i+1] = '\0';
-  current_map[0] = strtoul(astart, NULL, 16);
-  current_map[1] = strtoul(aend, NULL, 16);
-  return current_map;
-}
-
-int fill_remote_iovec(struct iovec  * remote, FILE *file){
+int fill_remote_iovec(struct iovec *remote, FILE *file) {
   char address_range[MAX_STR_LEN], perms[MAX_STR_LEN], pathname[MAX_STR_LEN];
   unsigned long offset;
   int dev_major, dev_minor, inode;
   int i = 0;
+  while (i < MAX_LINES &&
+         fscanf(file, "%s %s %lx %d:%d %d %[^\n]", address_range, perms,
+                &offset, &dev_major, &dev_minor, &inode, pathname) >= 4) {
+    printf("Filling remote[%d]...\n", i);
 
-  while (i < MAX_LINES && fscanf(file, "%49s %4s %lx %d:%d %d %49[^\n]", 
-    address_range, perms, &offset, &dev_major, &dev_minor, &inode, pathname) >= 5) {
-    uint64_t * current_map;
-    current_map = process_range(address_range);
-    uintptr_t start = current_map[0];
-    uintptr_t end = current_map[1];
+    uintptr_t start, end;
+    process_range(address_range, &start, &end);
+    printf("Start: 0x%lx\n", start);
+    printf("End: 0x%lx\n", end);
 
-    printf("Start: 0x%lx\n", (unsigned long)start);
-    printf("End:   0x%lx\n", (unsigned long)end);
+    ssize_t len = end - start;
+    printf("Length: 0x%zx\n", len);
+    remote[i].iov_base = (void *)start;
+    remote[i].iov_len = end - start;
+
     i++;
-    }
-    // Return the read mapped regions
-    return i;
+  }
+  return i;
 }
 
 int main(int argc, char **argv) {
   char *p;
-  pid_t user_input_pid = (pid_t) strtol(argv[1], &p, 10);
+  pid_t user_input_pid = (pid_t)strtol(argv[1], &p, 10);
   printf("Input PID = %d\n", user_input_pid);
-  char path [50];
+  char path[50];
   int rn = sprintf(path, "/proc/%d/maps", user_input_pid);
   printf("Opening %s ...\n", path);
-  FILE *file = fopen(path,"r");
-    if (!file) {
-      perror("Error opening file");
-      return 1;
+  FILE *file = fopen(path, "r");
+  if (!file) {
+    perror("Error opening file");
+    return 1;
   }
-  struct iovec remote[50];
-  fill_remote_iovec(remote, file);
+  struct iovec remote[500];
+  int n = fill_remote_iovec(remote, file);
+  struct iovec local[n];
+  // Build local iovec structs
+  for (int i = 0; i < n; i++) {
+    local[i].iov_base = calloc(remote[i].iov_len, sizeof(char));
+    local[i].iov_len = remote[i].iov_len;
+  }
+
+  ssize_t nread = process_vm_readv(user_input_pid, local, n, remote, n, 0);
+  if (nread < 0) {
+    switch (errno) {
+    case EINVAL:
+      printf("ERROR: Invalid arguments.\n");
+      break;
+    case EFAULT:
+      printf("ERROR: Unable to access target memory.\n");
+      break;
+    case ENOMEM:
+      printf("ERROR: Memory allocation failed.\n");
+      break;
+    case EPERM:
+      printf("ERROR: Insufficient privileges.\n");
+      break;
+    case ESRCH:
+      printf("ERROR: Process does not exist.\n");
+      break;
+    default:
+      printf("ERROR: Unknown error occurred.\n");
+    }
+    return 1;
+  }
+
+  printf("Read %zd bytes from %d regions.\n", nread, n);
+
   fclose(file);
   return 0;
 }
-
